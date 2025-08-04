08493aeac350dad4c7fcb333ab899f46f18f739e
Andrés <andres.herrera@alumnos.ucn.cl>
2025-08-04 18:20:27 -0400

    feat: implementar página de inicio completa

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,6 +5,8 @@ namespace NomadIndustriesWeb.Pages
 {
     public class IndexModel : PageModel
     {
+
+
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
@@ -16,5 +18,7 @@ namespace NomadIndustriesWeb.Pages
         {
 
         }
+
+
     }
 }
