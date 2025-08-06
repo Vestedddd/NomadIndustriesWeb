c7b7539b3031942a7422ff799848d142af4c9794
Andrés <andres.herrera@alumnos.ucn.cl>
2025-08-05 22:09:53 -0400

    feat: implementar página de fabricaciones completa y eliminar comentarios innecesarios

---
+++ Pages/Products/Sensores-iot.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NomadIndustriesWeb.Pages.Products
+{
+    public class SensoresIotModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,24 +1,26 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+
+builder.Services.AddRazorPages(options =>
+{
+    // productos
+    options.Conventions.AddPageRoute("/Products/Sensores-iot", "/productos");
+    options.Conventions.AddPageRoute("/Products/Sensores-iot", "/productos/sensores-iot");
+    
+
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapStaticAssets();
 app.MapRazorPages()
    .WithStaticAssets();
