a33417be79aa566b52e68ccbd9893059226c0fd2
Andrés <andres.herrera@alumnos.ucn.cl>
2025-08-07 16:17:25 -0400

    feat: implementar página sobre nosotros completa

---
+++ Pages/Sobre-nosotros.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NomadIndustriesWeb.Pages
+{
+    public class Sobre_nosotrosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
