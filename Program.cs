70629e541e12cd82c9ae137acfe686cbdd7fc4ce
Andrés <andres.herrera@alumnos.ucn.cl>
2025-09-04 00:18:24 -0400

    feat: implementar sistema completo de productos y actualizar página inicio

--- Program.cs
+++ Program.cs
@@ -1,28 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.AddRazorPages(options =>
 {
-    // productos
+   
+    options.Conventions.AddPageRoute("/Products/Sensores-iot", "/productos/lista");
     options.Conventions.AddPageRoute("/Products/Sensores-iot", "/productos");
-    options.Conventions.AddPageRoute("/Products/Sensores-iot", "/productos/sensores-iot");
+
     
-
+    options.Conventions.AddPageRoute("/Products/Detalle", "/productos/ver/{id}");
 });
-
 var app = builder.Build();
-
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 app.UseRouting();
 app.UseAuthorization();
 app.MapStaticAssets();
 app.MapRazorPages()
    .WithStaticAssets();
-
 app.Run();
